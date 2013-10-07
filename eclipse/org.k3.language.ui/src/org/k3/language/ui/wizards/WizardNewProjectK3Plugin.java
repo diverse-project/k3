7dcab05f00482371522be392fd149b282d994fdf
Vincent Fontanella <vincent.fontanella@inria.fr>
2013-10-07 13:56:26 +0200

    Modify generated pom.xml  for K3 project with ecore definition

--- eclipse/org.k3.language.ui/src/org/k3/language/ui/wizards/WizardNewProjectK3Plugin.java
+++ eclipse/org.k3.language.ui/src/org/k3/language/ui/wizards/WizardNewProjectK3Plugin.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
