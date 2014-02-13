d408e74b8852a44f212c01639ef0e90204b9cb18
Didier Vojtisek <didier.vojtisek@inria.fr>
2014-02-13 18:30:03 +0100

    minor cleaning

--- k3.eclipse/fr.inria.diverse.k3.ui/src/fr/inria/diverse/k3/ui/tools/classpath/ManageClasspathMaven.java
+++ k3.eclipse/fr.inria.diverse.k3.ui/src/fr/inria/diverse/k3/ui/tools/classpath/ManageClasspathMaven.java
@@ -3,7 +3,6 @@ package fr.inria.diverse.k3.ui.tools.classpath;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
--- k3.eclipse/fr.inria.diverse.k3.ui/src/fr/inria/diverse/k3/ui/wizards/pages/WizardPageCustomNewProjectK3Plugin.java
+++ k3.eclipse/fr.inria.diverse.k3.ui/src/fr/inria/diverse/k3/ui/wizards/pages/WizardPageCustomNewProjectK3Plugin.java
@@ -7,7 +7,6 @@ import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
