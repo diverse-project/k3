685437330d9f7d1c32bc2b8e9dad09b5fba73a49
dvojtise <didier.vojtisek@inria.fr>
2017-06-22 17:06:49 +0200

    added license headers
    (using releng tools when possible)

--- k3.eclipse/fr.inria.diverse.k3.al.annotationprocessor.plugin/src/fr/inria/diverse/k3/al/annotationprocessor/stepmanager/EventManagerRegistry.xtend
+++ k3.eclipse/fr.inria.diverse.k3.al.annotationprocessor.plugin/src/fr/inria/diverse/k3/al/annotationprocessor/stepmanager/EventManagerRegistry.xtend
@@ -1,4 +1,14 @@
-package fr.inria.diverse.k3.al.annotationprocessor.stepmanager
+/*******************************************************************************
+ * Copyright (c) 2017 Inria and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Inria - initial API and implementation
+ *******************************************************************************/
+ package fr.inria.diverse.k3.al.annotationprocessor.stepmanager
 
 import java.util.HashSet
 import java.util.Set
--- k3.eclipse/fr.inria.diverse.k3.ui.templates/src/fr/inria/diverse/k3/ui/templates/K3TemplateMessages.java
+++ k3.eclipse/fr.inria.diverse.k3.ui.templates/src/fr/inria/diverse/k3/ui/templates/K3TemplateMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2013 IBM Corporation and others.
+ * Copyright (c) 2006, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  *     IBM Corporation - initial API and implementation
  *     Code 9 Corporation - ongoing development
  *     Volker Wegert - bug 243087
+ *     Inria - adaptation for K3
  *******************************************************************************/
 package fr.inria.diverse.k3.ui.templates;
 
