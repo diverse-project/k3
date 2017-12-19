3763f25f70f4860ad60db8ab5bdb299f57866bd4
d-leroy <dorian.leroy@tuwien.ac.at>
2017-12-19 15:15:08 +0100

    Fixed default method of IStepManager

--- k3.eclipse/fr.inria.diverse.k3.al.annotationprocessor.plugin/src/fr/inria/diverse/k3/al/annotationprocessor/stepmanager/IStepManager.java
+++ k3.eclipse/fr.inria.diverse.k3.al.annotationprocessor.plugin/src/fr/inria/diverse/k3/al/annotationprocessor/stepmanager/IStepManager.java
@@ -2,11 +2,11 @@ package fr.inria.diverse.k3.al.annotationprocessor.stepmanager;
 
 public interface IStepManager {
 
-	default void executeStep(Object caller, StepCommand command, String className, String methodName) {
-		executeStep(caller, new Object[] {}, command, className, methodName);
-	};
+	void executeStep(Object caller, StepCommand command, String className, String methodName);
 
-	void executeStep(Object caller, Object[] parameters, StepCommand command, String className, String methodName);
+	default void executeStep(Object caller, Object[] parameters, StepCommand command, String className, String methodName) {
+		executeStep(caller, command, className, methodName);
+	};
 
 	boolean canHandle(Object caller);
 }
