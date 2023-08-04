1568bbad73b6d5079e63d1ecf7a2a2fb3044b740
Didier Vojtisek <didier.vojtisek@inria.fr>
2023-08-04 17:38:58 +0200

    fix basic template wizard

--- k3.eclipse/fr.inria.diverse.k3.ui/src/fr/inria/diverse/k3/ui/tools/K3FileTemplates.java
+++ k3.eclipse/fr.inria.diverse.k3.ui/src/fr/inria/diverse/k3/ui/tools/K3FileTemplates.java
@@ -20,13 +20,14 @@ public class K3FileTemplates {
 		StringBuffer buffer= new StringBuffer();
 		buffer.append("Manifest-Version: 1.0" + lineSeparator);
 		buffer.append("Bundle-ManifestVersion: 2" + lineSeparator);
+		buffer.append("Automatic-Module-Name: " + projectName + lineSeparator);
 		buffer.append("Bundle-Name: " + projectName + lineSeparator);
 		buffer.append("Bundle-SymbolicName: " + projectName + "; singleton:=true" + lineSeparator);
 		buffer.append("Bundle-Version: 1.0.0" + lineSeparator);
 		buffer.append("Require-Bundle: ");
-		buffer.append("fr.inria.diverse.k3.al.annotationprocessor.plugin;bundle-version=\"3.0.0\";visibility:=\"reexport\""+ lineSeparator);
+		buffer.append("fr.inria.diverse.k3.al.annotationprocessor.plugin;bundle-version=\"4.0.0\";visibility:=\"reexport\""+ lineSeparator);
 	    buffer.append("Bundle-ClassPath: ." + lineSeparator);
-	    buffer.append("Bundle-RequiredExecutionEnvironment: JavaSE-1.7"+lineSeparator);
+	    buffer.append("Bundle-RequiredExecutionEnvironment: JavaSE-17"+lineSeparator);
 	    
 	    return buffer.toString();
     }
